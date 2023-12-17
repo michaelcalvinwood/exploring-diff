090c9a025b7a2b0b79db7690346462bc5b04aa1b
Michael Wood <michaelwood33311@icloud.com>
2023-12-17 13:12:50 -0500

    created models list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonSelect, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.scss';
 import { useEffect, useState } from 'react';
@@ -7,18 +7,61 @@ import Diff from '../components/Diff';
 const Home = () => {
   const [orig, setOrig] = useState('beep boop');
   const [modified, setModified ] = useState('beep boob blah');
+  const [model, setModel] = useState('AcMod_01');
+
+  const models = [
+    {
+      id: 'AcMod_01',
+      name: 'Text Normalization'
+    },
+    {
+      id: 'AcMod_02',
+      name: 'CoReference Resolution'
+    },
+    {
+      id: 'AcMod_03',
+      name: 'Temporal Identification'
+    },
+    {
+      id: 'AcMod_04',
+      name: 'Relevance Determination'
+    },
+    {
+      id: 'AcMod_05',
+      name: 'AGI Extractor'
+    },
+    {
+      id: 'AcNLP_01',
+      name: 'Query Firewall'
+    },
+    {
+      id: 'AcNLP_02',
+      name: 'Temporal Time Correction'
+    },
+    {
+      id: 'AcNLP_03',
+      name: 'Quote Correction'
+    },
+    {
+      id: 'AcNLP_04',
+      name: 'Noun Phrase Reconciliation'
+    },
+    {
+      id: 'AcNLP_05',
+      name: 'Image and Citation Linkification'
+    }
+  ]
 
 
 
   
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+     
       <IonContent fullscreen>
+        <IonSelect>
+
+        </IonSelect>
         <div className='Home__diff'>
           <Diff  orig={orig} modified={modified} />
         </div>
